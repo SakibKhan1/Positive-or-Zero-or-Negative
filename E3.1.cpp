cd2159a9638d73d13894945881642b191422e0ac
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-03-05 20:33:31 -0500

    Create E3.1.cpp

---
+++ E3.1.cpp
@@ -0,0 +1,20 @@
+//Name: Sakib Khan
+//CSCI 135
+//Homework E3.1
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    if (number == 0)
+        cout << "zero" << endl;
+    if (number < 0)
+        cout << "negative" << endl;
+    if (number > 0)
+        cout << "positive" << endl;
+    return 0;
+}
+
